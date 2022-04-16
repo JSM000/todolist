f2e4556978870a3fb9ece1ca5f60f106b72431ea
JSM000 <dndls000@naver.com>
2022-04-16 19:51:10 +0900

    add todoitem

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-
+import Todoitem from "./Todoitem";
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray; /* 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
 `;
 
 function TodoList() {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <Todoitem />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
